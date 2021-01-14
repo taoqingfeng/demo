794c09bc287ec5ebc6acdba45dbd907b25e78e63
陶庆峰 <taoqingfeng@boe.com.cn>
2021-01-14 18:59:22 +0800

    jhjj

--- app.go
+++ app.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-14 16:59:15
- * @LastEditTime: 2021-01-14 18:57:18
+ * @LastEditTime: 2021-01-14 18:59:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demo\app.go
@@ -11,5 +11,5 @@
  import "fmt"
  
  func main(){
-		 fmt.Printf("HellosdfdsWorld!\n");
+		 fmt.Printf("HellosdsdffdsWorld!\n");
  }
